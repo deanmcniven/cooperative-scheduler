6bf2a2f56e61cfb5efb6048d4240f9cdf5400ba3
Dean McNiven <deanmcniven@users.noreply.github.com>
2016-05-10 13:17:43 +1000

    Create ISR to handle ticks on timer2

--- main.c
+++ main.c
@@ -7,12 +7,17 @@
 #define F_CPU 16000000
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include "stdio.h"
 
 int main(void)
 {
-    //Processor setup
-
+    //Setup Ticks generation
+    ASSR = 0x00;    //Timer2: Internal Clock
+    TCCR2A = 0x02;  //CTC Mode
+    TCCR2B = 0x07;  //Prescale: 1024
+    OCR2A = 0xEB;   //Count: 235 (~15ms)
+    TIMSK2 = 0x02;  //Enable Compare Interrupt
 
 
     /* Replace with your application code */
@@ -20,4 +25,10 @@ int main(void)
     {
 
     }
+}
+
+ISR(TIMER2_COMPA_vect)
+{
+    //Tick has occured
+    //schedule something?
 }
